14ff286f6fdebecaf5ccf47aa27178b0b4ede655
rorycl <rorycl>
2022-03-12 21:54:34 +0000

    files: add rmapi format file directory to files_test

--- files/files_test.go
+++ files/files_test.go
@@ -20,7 +20,9 @@ func ptime(ti string) time.Time {
 	return tp
 }
 
-func TestFilesWithPDF(t *testing.T) {
+// TestFilesXochitlWithPDF tests the xochitl file format for a test with
+// a backing pdf
+func TestFilesXochitlWithPDF(t *testing.T) {
 
 	template := ""
 	rmf, err := RMFiler("../testfiles/xochitl/cc8313bb-5fab-4ab5-af39-46e6d4160df3.pdf", template)
@@ -84,7 +86,8 @@ func TestFilesWithPDF(t *testing.T) {
 
 }
 
-func TestFilesWithoutPDF(t *testing.T) {
+// TestFilesXochitlWithoutPDF tests xochitl format files without a pdf
+func TestFilesXochitlWithoutPDF(t *testing.T) {
 
 	template := "../templates/A4.pdf"
 	rmf, err := RMFiler("../testfiles/xochitl/d34df12d-e72b-4939-a791-5b34b3a810e7", template)
@@ -138,5 +141,83 @@ func TestFilesWithoutPDF(t *testing.T) {
 	}
 	if rmf.Pages[0].LayerNames[0] != expected.Pages[0].LayerNames[0] {
 		t.Error("Page one second layer names not the same")
+	}
+}
+
+// TestFilesRMapiWithPDF tests the xochitl file format for a test with
+// a backing pdf
+func TestFilesRMapiWithPDF(t *testing.T) {
+
+	template := ""
+	rmf, err := RMFiler("../testfiles/rmapi/c73a0dfe-00e3-4f41-8f17-27136804069a.pdf", template)
+	if err != nil {
+		t.Errorf("Could not open file %v", err)
+	}
+
+	expected := RMFileInfo{
+		RelPDFPath:   "../testfiles/rmapi/c73a0dfe-00e3-4f41-8f17-27136804069a.pdf",
+		Identifier:   "c73a0dfe-00e3-4f41-8f17-27136804069a",
+		Version:      0,                                      // no version
+		VisibleName:  "",                                     // none provide for rmapi
+		LastModified: ptime("0001-01-01 00:00:00 +0000 UTC"), // none provided for rmapi
+		PageCount:    3,
+		Pages: []RMPage{
+			{
+				PageNo:     0,
+				Identifier: "0-metadata",
+				RelRMPath:  "../testfiles/rmapi/c73a0dfe-00e3-4f41-8f17-27136804069a/0.rm",
+				Exists:     true,
+				LayerNames: []string{"Layer 1"},
+			},
+			{
+				PageNo:     1,
+				Identifier: "1-metadata",
+				RelRMPath:  "../testfiles/rmapi/c73a0dfe-00e3-4f41-8f17-27136804069a/1.rm",
+				Exists:     false,
+				LayerNames: []string{},
+			},
+			{
+				PageNo:     2,
+				Identifier: "2-metadata",
+				RelRMPath:  "../testfiles/rmapi/c73a0dfe-00e3-4f41-8f17-27136804069a/2.rm",
+				Exists:     true,
+				LayerNames: []string{"Layer 1"},
+			},
+		},
+		UseTemplate: false,
+		Debugging:   false,
+	}
+
+	if rmf.RelPDFPath != expected.RelPDFPath {
+		t.Errorf("RelPDFPath wanted %v got %v", rmf.RelPDFPath, expected.RelPDFPath)
+	}
+	if rmf.Identifier != expected.Identifier {
+		t.Errorf("Identifier wanted %v got %v", rmf.Identifier, expected.Identifier)
+	}
+	if rmf.Version != expected.Version {
+		t.Errorf("Version wanted %v got %v", rmf.Version, expected.Version)
+	}
+	if rmf.VisibleName != expected.VisibleName {
+		t.Errorf("VisibleName wanted %v got %v", rmf.VisibleName, expected.VisibleName)
+	}
+	if rmf.PageCount != expected.PageCount {
+		t.Errorf("PageCount wanted %v got %v", rmf.PageCount, expected.PageCount)
+	}
+
+	if rmf.Pages[0].PageNo != expected.Pages[0].PageNo {
+		t.Errorf("Page one PageNo wanted %v got %v", rmf.Pages[0].PageNo, expected.Pages[0].PageNo)
+	}
+	if rmf.Pages[0].Identifier != expected.Pages[0].Identifier {
+		t.Errorf("Page one Identifier wanted %v got %v", rmf.Pages[0].Identifier, expected.Pages[0].Identifier)
+	}
+	if rmf.Pages[0].RelRMPath != expected.Pages[0].RelRMPath {
+		t.Errorf("Page one RelRMPath wanted %v got %v", rmf.Pages[0].RelRMPath, expected.Pages[0].RelRMPath)
+	}
+	if rmf.Pages[0].LayerNames[0] != expected.Pages[0].LayerNames[0] {
+		t.Error("Page one second layer names not the same")
+	}
+
+	if len(rmf.Pages) != len(expected.Pages) {
+		t.Errorf("Number of pages wanted %v got %v", len(rmf.Pages), len(expected.Pages))
 	}
 }
