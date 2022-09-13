dd8c29143d7009385681eb68523b7da4dc2b421e
rorycl <gitrl@campbell-lange.net>
2022-09-13 15:14:10 +0100

    interim commit

---
+++ penconfig/penconfig.go
@@ -0,0 +1,132 @@
+// Package penconfig allows custom specification of pen strokes from a
+// yaml format file on a per-layer basis, for example
+//
+//   all:
+//     - pen:     fineliner
+//       weight:  narrow
+//       width:   0.95
+//       color:   black
+//       opacity: 0.9
+//
+//   "1":
+//     - pen:     fineliner
+//       weight:  narrow
+//       width:   0.8
+//       color:   blue
+//       opacity: 0.8
+//
+package penconfig
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+// penTypes are the currently understood pen types
+var penTypes = []string{
+	"pen",
+	"fineliner",
+	"marker",
+	"highligher",
+	"eraser",
+	"sharp pencil",
+	"erase area",
+	"paint",
+	"mechanical pencil",
+	"pencil",
+	"ballpoint",
+	"marker",
+	"pen",
+	"highlighter",
+}
+
+// penWeights are the currently understood pen weights
+var penWeights = []string{"narrow", "standard", "broad"}
+
+// PenConfig allows the configuration of a s
+type PenConfig struct {
+	Pen     string  `yaml:"pen"`
+	Weight  string  `yaml:"weight"`
+	Width   float32 `yaml:"width"`
+	Colour  string  `yaml:"color"`
+	Opacity float64 `yaml:"opacity"`
+}
+
+// LayerPenConfigs defines StrokeSettings by layer
+type LayerPenConfigs map[string][]PenConfig
+
+// LoadYaml reads bytes into a PenConfig structure
+func LoadYaml(yamlByte []byte) (LayerPenConfigs, error) {
+
+	var lpc LayerPenConfigs
+	err := yaml.Unmarshal(yamlByte, &lpc)
+	if err != nil {
+		return lpc, err
+	}
+	err = lpc.check()
+	return lpc, err
+}
+
+// check checks the validity of the configuration file
+func (lpc LayerPenConfigs) check() error {
+
+	// layers to list of pens
+	for layer, penList := range lpc {
+		if layer != "all" {
+			_, err := strconv.Atoi(layer)
+			if err != nil {
+				return fmt.Errorf("penconfig layer %s needs to be 'all' or a layer number", layer)
+			}
+		}
+
+		// list of pen configs in layer
+		for i, pen := range penList {
+
+			// check pen type
+			penOK := false
+			for _, penType := range penTypes {
+				if pen.Pen == penType {
+					penOK = true
+					break
+				}
+			}
+			if !penOK {
+				return fmt.Errorf(
+					"layer %s, item %d pen type %s not in\n%s",
+					layer, i, pen.Pen, strings.Join(penTypes, " "),
+				)
+			}
+
+			// check pen weight (should be checked by pen type too)
+			weightOK := false
+			for _, weight := range penWeights {
+				if pen.Weight == weight {
+					weightOK = true
+					break
+				}
+			}
+			if !weightOK {
+				fmt.Printf("error pen %+v\n", pen)
+				return fmt.Errorf(
+					"layer %s, item %d weight type %s not in \n%s",
+					layer, i, pen.Weight, strings.Join(penWeights, " "),
+				)
+			}
+
+			// check pen opacity
+			if pen.Opacity < 0.0 || pen.Opacity > 1.0 {
+				return fmt.Errorf("layer %s, item %d opacity %f invalid", layer, i, pen.Opacity)
+			}
+
+			// check pen width
+			if pen.Width < 0.0 || pen.Width > 30.0 {
+				return fmt.Errorf("layer %s, item %d width %f invalid", layer, i, pen.Width)
+			}
+
+		}
+	}
+	return nil
+}
---
+++ penconfig/penconfig_test.go
@@ -0,0 +1,80 @@
+package penconfig
+
+// colours to be dealt with by
+// https://github.com/go-playground/colors or LocalColour parser
+
+import (
+	"strings"
+	"testing"
+)
+
+// TestPenConfigParse tests basic parsing
+func TestPenConfigParse(t *testing.T) {
+
+	y := []byte(`
+all:
+  - pen:     fineliner
+    weight:  narrow
+    width:   0.95
+    color:   black
+    opacity: 0.9
+
+"1":
+  - pen:     fineliner
+    weight:  narrow
+    width:   0.8
+    color:   blue
+    opacity: 0.8`)
+
+	lpc, err := LoadYaml(y)
+	if err != nil {
+		t.Errorf("load config error %s : %s", y, err)
+	}
+	t.Log(lpc)
+
+}
+
+// TestPenConfigParseFail1 tests basic parsing
+func TestPenConfigParseFail1(t *testing.T) {
+
+	y := []byte(`
+"fail":
+  - pen:     fineliner
+    weight:  narrow
+    width:   0.8
+    color:   blue
+    opacity: 0.8`)
+
+	_, err := LoadYaml(y)
+	if err == nil {
+		t.Error("load config should error with invalid layer name")
+	}
+	if !strings.Contains(err.Error(), "layer fail") {
+		t.Error("error should contain 'layer fail'")
+	}
+	expected := "layer fail"
+	if !strings.Contains(err.Error(), expected) {
+		t.Errorf("error should contain '%s'", expected)
+	}
+}
+
+// TestPenConfigParseFail2 tests basic parsing
+func TestPenConfigParseFail2(t *testing.T) {
+
+	y := []byte(`
+"all":
+  - pen:     nonsense
+    weight:  narrow
+    width:   0.8
+    color:   blue
+    opacity: 0.8`)
+
+	_, err := LoadYaml(y)
+	if err == nil {
+		t.Error("load config should error with invalid pen name")
+	}
+	expected := "pen type nonsense not in"
+	if !strings.Contains(err.Error(), expected) {
+		t.Errorf("error should contain '%s'", expected)
+	}
+}
